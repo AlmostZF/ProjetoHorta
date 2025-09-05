546b6c320a5e0790551436b404e59fa40b81cc10
Guilherme <guilhermegarcia487@gmail.com>
2025-09-05 17:03:14 -0300

    fix style

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,12 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+
 import { provideHttpClient } from '@angular/common/http';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(),provideHttpClient(), HttpClientModule
+    provideServerRendering(),provideHttpClient()
   ]
 };
 
