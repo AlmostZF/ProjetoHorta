a4f1dd2b6e1860b8b8d443594c939dab290b61e5
Guilherme <guilhermegarcia487@gmail.com>
2025-09-05 13:46:05 -0300

    update version

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,7 +1,7 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 const serverConfig: ApplicationConfig = {
   providers: [
